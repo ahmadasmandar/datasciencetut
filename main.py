b9d4a9727d381c60d481fc6fbd825a2f1d630ab8
ahmadasmndr <ahmedasmndr2@gmail.com>
2019-09-04 02:40:42 +0200

    init to first push on git

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+This Document was created by Ahmad ASMANDAR ON 04.09.2019
+and is being used to learn data-science with Python
+"""
+# Here is the USED libs pandas, matplotlib...
+import pandas as pd
+from pandas import DataFrame as df
+import matplotlib.pyplot as plt
+
+# read csv file using pandas
+
+data = pd.read_csv("data.csv")
+# use datframes from pandas to extract data from tables
+
+X = df(data, columns=['production_budget_usd'])
+y = df(data, columns=['worldwide_gross_usd'])
+
+# use matplot lib to visualise our data
+
+# set the figure size
+plt.figure(figsize=(10, 6))
+
+# the data to plot, using the scatter plot function
+# and using the alpha parameter to determine the destiny of the Data in a point
+plt.scatter(X, y, alpha=0.4)
+
+# the labels to title,x and y labels
+plt.title("first data-science panda example")
+plt.xlabel('production_budget_usd')
+plt.ylabel('world_wide_gross')
+
+
+plt.xlim(0, )
+plt.ylim(0, )
+plt.show()
